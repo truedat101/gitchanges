6c673f0dd26232504d798965ac23a484138b993e
David J. Kordsmeier <dkords@gmail.com>
2014-06-14 16:30:15 -0700

    Cleanup whatever stupid thing I did

--- gitchanges.git/gitchanges.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-BASEDIR=$(dirname $0)
-echo "Changes between $1 and $2";
-# git log $1..$2 --pretty=format:"* %s" --reverse | grep -v Merge
-git log $1..$2 --pretty=format:"* %s" | grep -v Merge
